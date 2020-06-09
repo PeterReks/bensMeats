ea611266dbca4b2663d5c185081c497159a8fd82
PeterReks <64718863+PeterReks@users.noreply.github.com>
2020-06-09 15:00:15 -0600

    Add files via upload

--- docs/script.js
+++ docs/script.js
@@ -1,11 +1,19 @@
-var hamMenu = document.getElementById("hamburger");
-var toggleNav = document.getElementById("mobileNav");
+var hamMenu = document.querySelector("#hamburger");
+var hamOpen = document.querySelector("#mobileNav");
 
-  hamMenu.addEventListener("click", () =>  {
-    if (toggleNav.style.display === "none") {
-        toggleNav.style.display = "flex";
-      } else {
-        toggleNav.style.display = "none";
-      }
+hamMenu.addEventListener("click", () => {
+  if (hamMenu.style.display === "block") {
+    hamOpen.style.display = "flex";
+    hamMenu.style.display = "none";
+  } else {
+    hamMenu.style.display = "block"
+  }
+});
+
+  hamOpen.addEventListener("click", () => {
+    if (hamMenu.style.display === "none") {
+      hamMenu.style.display = "block";
+      hamOpen.style.display = "none";
+    }
 
 });
